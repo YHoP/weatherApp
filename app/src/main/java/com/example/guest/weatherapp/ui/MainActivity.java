de480ee8f3de3de35360ea85807c5a60c4cd7369
yvonne peng & morgan lutz <student@epicodus.com>
2015-11-02 13:27:29 -0800

    update design view

--- app/src/main/java/com/example/guest/weatherapp/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/weatherapp/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.guest.weatherapp.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethod;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText cityName;
     TextView txtResult;
+    Button btnSubmit;
 
     public void findWeather(View view) {
 
@@ -59,6 +62,17 @@ public class MainActivity extends AppCompatActivity {
 
         cityName = (EditText) findViewById(R.id.txtCityName);
         txtResult = (TextView) findViewById(R.id.txtResult);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+                intent.putExtra("cityName", cityName.getText().toString());
+                startActivity(intent);
+            }
+        });
+
 
 
 
@@ -144,25 +158,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/example/guest/weatherapp/ui/WeatherActivity.java
+++ app/src/main/java/com/example/guest/weatherapp/ui/WeatherActivity.java
@@ -1,39 +1,139 @@
 package com.example.guest.weatherapp.ui;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
 
 import com.example.guest.weatherapp.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class WeatherActivity extends AppCompatActivity {
+
+    String cityName;
+
+    public void findWeather(View view) {
+
+
+        try {
+            String encodedCityName = URLEncoder.encode(cityName, "UTF-8");
+            DownloadTask task = new DownloadTask();
+            task.execute("http://api.openweathermap.org/data/2.5/weather?q=" + encodedCityName + "&APPID=482e2d21c007b79e42312c67d47cd01a");
+
+        } catch (UnsupportedEncodingException e) {
+            Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        cityName = extras.getString("cityName");
+        Toast.makeText(getApplicationContext(), "City Name: " + cityName, Toast.LENGTH_LONG).show();
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_weather, menu);
-        return true;
-    }
+    public class DownloadTask extends AsyncTask<String, Void, String> {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            try {
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+                }
+
+                return result;
+
+            } catch (Exception e) {
+
+                Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG).show();
+
+            }
+            return null;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+
+                String message = "";
+
+                JSONObject jsonObject = new JSONObject(result);
+                String weatherInfo = jsonObject.getString("weather");
+                Log.i("Weather content", weatherInfo);
+
+                JSONArray arr = new JSONArray(weatherInfo);
+
+                for (int i = 0; i <arr.length(); i++) {
+                    JSONObject jsonPart = arr.getJSONObject(i);
+
+                    String main = "";
+                    String description = "";
+
+                    main = jsonPart.getString("main");
+                    description = jsonPart.getString("description");
+
+                    if(main != "" && description != "") {
+                        message += main + ": " + description + "\r\n";
+                    }
+
+                }
+
+                if(message != "") {
+//                    txtResult.setText(message);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Could not find the weather. Please try again.", Toast.LENGTH_LONG).show();
+                }
+
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "Could not find the weather. Please try again.", Toast.LENGTH_LONG).show();
+
+            }
+
+        }
     }
 }
